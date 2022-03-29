0b17d282a631ca04f9c2b582cec8f50a01da9c1a
Leom.json <89754898+Leomjson@users.noreply.github.com>
2022-03-29 17:24:46 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-/*
-                                        -- Modified by GuckTubeYT#3123
-                                        -- Coded by Clayne#0001
-                                        --  Helped by No#7777
-
-                                            ^^^^^^^^^^^^^^^^^^^
-Leomjson made it Discord.js V13 Supportive. (give credit above)
-Discord Tag | leom#0001
-
-READ! I did not steal any credit, I forked GuckTubeYT to the repo and even said that its not my work-
-do not accuse me of copyrighting, I only wanted to make this support V13 of discord.js and share my work!
-*/
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
+//
+//
+//
+const TOKEN = 'bot token';
+const CHANNELID = 'channel ID';
+//
+//
+//
+const { Client, Intents, MessageEmbed, Permissions } = require('discord.js');
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]
+});
 var fs = require('fs');
 const exec = require('child_process').exec;
 const lineReader = require('line-reader');
@@ -22,51 +19,12 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 console.clear();
-
-if (!fs.existsSync("appconfig.json"))
-{
-    let tokendiscordbot;
-    let channeliddiscordbot;
-    let playerfoldername;
-    let worldfoldername;
-    let onlineplayertxt;
-    rl.question("Your Token Discord Bot = ", function(token) {
-        rl.question("Your Channel Discord = ", function(channel) {
-            rl.question("your players folder name = ", function(player) {
-                rl.question("your worlds folder name = ", function(world) {
-                    rl.question("your online player file name = ", function(onlineplayer) {
-                        rl.question("your exe gtps file name = ", function(exe) {
-                        
-                        rl.close();
-                        fs.writeFileSync("appconfig.json", `{
-                            "token" : "${token}",
-                            "channel" : "${channel}",
-                            "player" : "${player}",
-                            "world" : "${world}",
-                            "onlineplayer" : "${onlineplayer}",
-                            "exegtps" : "${exe}"
-                        }`)
-                        console.log("GTPS Status Discord Bot Has been Setted Up!")
-                        console.log("Starting Bot...")
-                        start();
-                        });
-                        
-        });
-        });
-        });
-        });
-        });
-}
-else
-{
     start();
-}
     function start()
     {
-    const config = require("./appconfig.json")
-    const isRunning = (query, cb) => {
+    const isRunning = (query, cb) => 
+    {
         let platform = process.platform;
         let cmd = '';
         switch (platform) {
@@ -79,77 +37,80 @@ else
             cb(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
         });
     }
-    
     client.on('ready', () => {
+        const Guilds = client.guilds.cache.map(guild => guild.id);
+        client.user.setActivity(`In ${client.guilds.cache.size} Servers`, 
+        {
+            type: "STREAMING",
+            url: "https://www.twitch.tv/leo"
+        });
 
-        const statusz = new MessageEmbed()
-        .setColor('#ff0000')
-        .setAuthor({ name: 'GT Private Server' })
-        .addField('*Server Status:**', '**DOWN**')
-        .addField('Players Online:', 'Please wait.')
-        .setTimestamp()
-        .setFooter({ text: 'Last Updated' });
-        client.channels.cache.get(config.channel).send({ embeds: [statusz] }).then((msg)=> {
+//
+//  
+const channel = client.channels.cache.get(CHANNELID);
+//
+//
+
+        const embedsd = new MessageEmbed()
+        .setTitle('Checking Status...');
+        channel.send({ embeds: [embedsd] }).then((msg)=> {
     
-      setInterval(function(){
-          var color = randomColor();
-    isRunning(`${config.exegtps}`, (status) => {
+      setInterval(function() {
+        var color = randomColor();
+    isRunning('Growtopia.exe', (status) => {
         if (status == true) {
-            if (!fs.existsSync(config.onlineplayer))
+            if (!fs.existsSync('online_count.txt'))
             {
-            const f1 = fs.readdirSync(config.player).length
-            const f2 = fs.readdirSync(config.world).length
-
+            const f1 = fs.readdirSync('save/players/').length
+            const f2 = fs.readdirSync('save/worlds/').length
         const statuszz = new MessageEmbed()
         .setColor(color)
-        .setAuthor({ name: `${msg.guild.name}` })
-        .addField('**Server Status:**', '**UP**')
-        .addField('**Players File Count: **', f1)
-        .addField('**Worlds File Count: **', f2)
-        .setTimestamp()
-        .setFooter({ text: 'Last Updated' });
-        
-            msg.edit(statuszz);
+        .addFields
+        (
+        { name: 'Server Status:     ⠀', value: '**UP**', inline: false },
+        { name: 'Online Count: ', value: '**Problem Loading**',  inline: false },
+        { name: 'Player Files:     ⠀', value: '**' + f1 + '**', inline: false },
+        { name: 'World Files:     ⠀', value: '**' + f2 + '**', inline: false },
+        )
+        .setTimestamp();
+            msg.edit({embeds: [statuszz]});
             }
-            else
-            {
-            lineReader.eachLine(config.onlineplayer, function(line) {
-            const f1 = fs.readdirSync(config.player).length
-            const f2 = fs.readdirSync(config.world).length
-
-            const statuszz = new MessageEmbed()
-            .setColor(color)
-            .setAuthor({ name: `${msg.guild.name}` })
-            .addField('**Server Status:**', '**UP**')
-            .addField('**Players Online:**', line)
-            .addField('**Players File Count: **', f1)
-            .addField('**Worlds File Count: **', f2)
-            .setTimestamp()
-            .setFooter({ text: 'Last Updated' });
-            msg.edit(statuszz);
+            else {
+               lineReader.eachLine('online_count.txt', function(line) {
+                const f1 = fs.readdirSync('players/').length
+                const f2 = fs.readdirSync('worlds/').length
+               const statuszz = new MessageEmbed()
+               .setColor(color)
+               .addFields
+               (
+                { name: 'Server Status:     ⠀', value: '**UP**', inline: false },
+                { name: 'Online Count: ', value: '**' + line + '**',  inline: false },
+                { name: 'Player Files:     ⠀', value: '**' + f1 + '**', inline: false },
+                { name: 'World Files:     ⠀', value: '**' + f2 + '**', inline: false },
+                )
+                .setTimestamp();
+                    msg.edit({embeds: [statuszz]});
     });
-}
+            }
         }
-        else
-        {
-            const f1 = fs.readdirSync(config.player).length
-            const f2 = fs.readdirSync(config.world).length
-            const statusz = new MessageEmbed()
+        else {
+            const f1 = fs.readdirSync('players/').length
+            const f2 = fs.readdirSync('worlds/').length
+        const statusz = new MessageEmbed()
         .setColor(color)
-        .setAuthor({ name: `${msg.guild.name}` })
-        .addField('**Server Status:**', '**DOWN**')
-        .addField('**Players online:**', '0')
-            .addField('**Players File Count: **', f1)
-        .addField('**Worlds File Count: **', f2)
-        .setTimestamp()
-        .setFooter({ text: 'Last Updated' });
-         
-             msg.edit(statusz);
+        .addFields
+        (
+        { name: 'Server Status:     ⠀', value: '**DOWN**', inline: false },
+        { name: 'Online Count: ', value: '**0**',  inline: false },
+        { name: 'Player Files:     ⠀', value: '**' + f1 + '**', inline: false },
+        { name: 'World Files:     ⠀', value: '**' + f2 + '**', inline: false },
+        )
+        .setTimestamp();
+            msg.edit({embeds: [statusz]});
         }
     })
-      }, 8000) // ~Leom don't lower this, else it will lag.
-    }); 
+      }, 6000)
+    });
     })
-    
-    client.login(config.token)
+    client.login(TOKEN)
     }
